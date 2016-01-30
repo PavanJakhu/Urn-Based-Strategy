dce42b5e2480bcdb2d6e7e85bd35839e7a76569f
A Caskanette <apcaskanette@gmail.com>
2016-01-30 00:10:22 -0500

    Fixed Camera Movement (though it still bounces on top of character)

---
+++ Source/Urn_Based/UrnUnit.cpp
@@ -0,0 +1,143 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Urn_Based.h"
+#include "UrnUnit.h"
+
+
+// Sets default values
+AUrnUnit::AUrnUnit()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AUrnUnit::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AUrnUnit::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+// Called to bind functionality to input
+void AUrnUnit::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	Super::SetupPlayerInputComponent(InputComponent);
+
+}
+
+
+// Set Starting Stats
+
+void AUrnUnit::SetStartingStats(int32 hp, int32 spd, int32 ap, int32 prw, int32 acc, int32 str, int32 end, int32 agi, int32 wll, int32 intel, int32 arm, int32 sz) {
+
+	// Death prep procedures here
+	hitPoints = hp;			// How much health you have left before dying
+	maxHitPoints = hp;		// Maximum health you can heal back up to
+	speed = spd;					// How far you can move in meters per Action Point
+	actionPoints = ap;	    // Action Points allow you to take actions per turn
+	maxActionPoints = ap;  // Action Points gained per turn
+	powerPoints = intel * 2;      // Power points allow you to cast spells, each spell has a specific power point cost
+	maxPowerPoints = intel * 2;					// How many Power points you can have total
+	prowess = prw;          // Determines likelihood to hit and be hit in melee
+	accuracy = acc;         // Determines likelihood to hit enemies at range
+	strength = str;         // Determines power with which you hit enemies in melee
+	endurance = end;        // Determines how resistant you are to status effects
+	agility = agi;          // Determines how well you can dodge attacks that reflexes would assist with
+	willpower = wll;        // Determines likelihood you are to retreat and how many Power Points you regenerate each turn
+	intelligence = intel;     // Determines strength of spells
+	armour = arm;           // Determines how resistant to damage you are
+	size = sz;             // Determines how big you are, therefore what you can hide behind
+
+
+}
+
+
+/// Hit Point Management
+
+void AUrnUnit::AddHealth(int32 deltaHealth) {
+
+	hitPoints += deltaHealth;
+	if (hitPoints > maxHitPoints)
+		hitPoints = maxHitPoints;
+	else if (hitPoints <= 0)
+		OutOfHealth();
+
+}
+
+
+void AUrnUnit::OutOfHealth() {
+
+	// Death prep procedures here
+
+}
+
+
+void AUrnUnit::CalculateDamageTaken(FAttackInfo attackInfo) {
+
+	int32 damageTaken = attackInfo.GetAttackStrength(); // base damage sent by attacker
+	damageTaken -= armour;                              // reduced by Armour
+	AddHealth(-damageTaken);
+
+}
+
+
+// Action Point Management
+
+bool AUrnUnit::PerformAction(EActionType Action_, int32 APCost_) {
+
+	if (CanSpendActionPoints(APCost_)) {
+
+		switch (Action_)
+		{
+		case EActionType::AT_WALK:
+			break;
+		case EActionType::AT_RUN:
+			break;
+		case EActionType::AT_ATTACK:
+			break;
+		default:
+			break;
+		}
+
+		SpendActionPoints(APCost_);
+
+		return true;
+
+	}
+
+	return false;
+
+}
+
+
+bool AUrnUnit::CanSpendActionPoints(int32 APCost_) {
+	// Are my current actionPoints good to go?	
+	return (actionPoints >= APCost_);
+}
+
+
+void AUrnUnit::SpendActionPoints(int32 APSpent_) {
+
+	// Spend AP
+	actionPoints = actionPoints - APSpent_;
+	// Don't accidentally go negative
+	if (actionPoints < 0)
+		actionPoints = 0;
+
+}
+
+void AUrnUnit::RefreshActionPoints() {
+
+	// Set Action Points back to maximum
+	actionPoints = maxActionPoints;
+
+}
+
--- Source/Urn_Based/Urn_BasedCharacter.h
+++ Source/Urn_Based/Urn_BasedCharacter.h
@@ -4,63 +4,6 @@
 #include "Urn_BasedCharacter.generated.h"
 
 
-UENUM(BlueprintType)		//"BlueprintType" is essential to include
-enum class EActionType
-{
-	AT_WALK 		UMETA(DisplayName = "Walk"),
-	AT_RUN 			UMETA(DisplayName = "Run"),
-	AT_ATTACK		UMETA(DisplayName = "Attack")
-};
-
-UENUM(BlueprintType)
-enum class EAttackType : uint8 {
-	ATT_MELEE 		UMETA(DisplayName = "Melee"),
-	ATT_RANGED 		UMETA(DisplayName = "Ranged"),
-	ATT_MAGIC 		UMETA(DisplayName = "Magic")
-};
-
-
-USTRUCT(BlueprintType)
-struct FAttackInfo
-{
-	GENERATED_USTRUCT_BODY()
-
-	UPROPERTY()
-		EAttackType attackType;
-
-	UPROPERTY()
-		int32 attackStrength;
-
-	UPROPERTY()
-		AActor* attackingActor;
-
-	//Set Attack Strength
-	void SetAttackStrength(const int32 AttackStrength_)
-	{
-		attackStrength = AttackStrength_;
-	}
-	//Get Attack Strength
-	int32 GetAttackStrength() {
-		return attackStrength;
-	}
-
-	//Get Attacking Actor
-	AActor* GetAttackingActor()
-	{
-		if (attackingActor->IsValidLowLevel())
-			return attackingActor;
-		else
-			return NULL;
-	}
-
-	//Constructor
-	FAttackInfo()
-	{
-		attackType = EAttackType::ATT_MELEE;
-		attackStrength = 10;
-		attackingActor = NULL;
-	}
-};
 
 
 
@@ -87,74 +30,7 @@ public:
 	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 
 	
-	// Set Stats
-	UFUNCTION(BlueprintCallable, Category = "OnStart")
-		void SetStartingStats(int32 hp = 10, int32 spd = 3, int32 ap = 3, int32 prw = 10, int32 acc = 10, int32 str = 10, int32 end = 10, int32 agi = 10, int32 wll = 10, int32 intel = 10, int32 arm = 10, int32 sz = 3);
-
-
-	// Hit Point Management
-
-	UFUNCTION(BlueprintCallable, Category = "Health")
-		void AddHealth(int32 deltaHP);
-	UFUNCTION(BlueprintCallable, Category = "Health")
-		void OutOfHealth();
-
-	//Damage Calculations
-
-	UFUNCTION(BlueprintCallable, Category = "Damage")
-		virtual void CalculateDamageTaken(FAttackInfo attackInfo);
-
-	// Action Points
-
-	UFUNCTION(BlueprintCallable, Category = "Actions")
-		bool PerformAction(EActionType Action_, int32 APCost_);
-	UFUNCTION(BlueprintCallable, Category = "Actions")
-		bool CanSpendActionPoints(int32 APCost_);
-	UFUNCTION(BlueprintCallable, Category = "Actions")
-		void SpendActionPoints(int32 APSpent_);
-	UFUNCTION(BlueprintCallable, Category = "Actions")
-		void RefreshActionPoints();
-
-
-
-
-	// Properties - Character Statistics
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Stats")
-		int32 hitPoints;        // How much health you have left before dying
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Stats")
-		int32 maxHitPoints;     // Maximum health you can heal back up to
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Stats")
-		int32 speed;            // How far you can move in meters per Action Point
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Stats")
-		int32 actionPoints;	    // Action Points allow you to take actions per turn
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Stats")
-		int32 maxActionPoints;  // Action Points gained per turn
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Stats")
-		int32 powerPoints;      // Power points allow you to cast spells, each spell has a specific power point cost
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Stats")
-		int32 maxPowerPoints;   // How many Power points you can have total
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Stats")
-		int32 prowess;          // Determines likelihood to hit and be hit in melee
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Stats")
-		int32 accuracy;         // Determines likelihood to hit enemies at range
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Stats")
-		int32 strength;         // Determines power with which you hit enemies in melee
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Stats")
-		int32 endurance;        // Determines how resistant you are to status effects
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Stats")
-		int32 agility;          // Determines how well you can dodge attacks that reflexes would assist with
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Stats")
-		int32 willpower;        // Determines likelihood you are to retreat and how many Power Points you regenerate each turn
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Stats")
-		int32 intelligence;     // Determines strength of spells
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Stats")
-		int32 armour;           // Determines how resistant to damage you are
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Stats")
-		int32 size;             // Determines how big you are, therefore what you can hide behind
-
-
-
+	
 
 };
 
