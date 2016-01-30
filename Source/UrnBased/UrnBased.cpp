e12ab81f4fe8816a64e96861c45d15e0d4fdfb5c
Pavan Jakhu <PavanJakhu@gmail.com>
2016-01-29 19:46:41 -0500

    Cleaned

--- Source/UrnBased/UrnBased.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
-
-#include "UrnBased.h"
-
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UrnBased, "UrnBased" );
-
-DEFINE_LOG_CATEGORY(LogUrnBased)
- 
